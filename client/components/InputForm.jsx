b3572e7b34c100cba0bdf37d9ce0a4d948af0357
Jake Johnson <jkjohnson1996@gmail.com>
2023-11-04 15:59:42 -0700

    react update with fetch

--- client/components/InputForm.jsx
+++ client/components/InputForm.jsx
@@ -4,6 +4,15 @@ function InputForm() {
   return (
     <div className="input-form" enctype="multipart/form-data">
       {/* Form elements go here */}
+      <label className='label age'>Enter your pet's name:
+        <input type="text" placeholder='Name'/>
+      </label>
+      <label className='label name'>Enter your pet's age:
+        <input type="text" placeholder='Age'/>
+      </label>
+      <label className='label desc'>Briefly describe your pet:
+        <textarea type="text" placeholder='Description' rows={5}/>
+      </label>
       <button className='submitBtn'>Submit Pet</button>
     </div>
   );
