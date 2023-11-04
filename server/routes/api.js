5f655f02197529227c4080194e4759c99c8a00d0
mai033 <145622463+mai033@users.noreply.github.com>
2023-11-04 15:38:01 -0700

    commit 3

--- server/routes/api.js
+++ server/routes/api.js
@@ -1,10 +1,9 @@
 const express = require('express');
-const petController = require('../controllers/petController')
+const petController = require('../controllers/petController');
 const router = express.Router();
 
+router.get('/', petController.getPets);
 
-
-
-
+router.post('/', petController.addPet);
 
 module.exports = router;
