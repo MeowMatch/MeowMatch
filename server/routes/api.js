0b8017d06a8f25a9fbc3d9cb21d335bc229b4384
eteering <teeringe@gmail.com>
2023-11-04 17:07:05 -0600

    what

--- server/routes/api.js
+++ server/routes/api.js
@@ -2,19 +2,8 @@ const express = require('express');
 const petController = require('../controllers/petController');
 const router = express.Router();
 
-<<<<<<< HEAD
-router.patch('/:name', petController.updatePet, (req, res) => {
-    res.status(200).json(res.locals.updatedPet);
-  });
-
-router.get('/:name', petController.updatePet, (req, res) => {
-    res.status(200).json(pets);
-  });
-
-=======
 router.get('/', petController.getPets);
 
 router.post('/', petController.addPet);
->>>>>>> dev
 
 module.exports = router;
