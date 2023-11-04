ea8f83a389e3f0c6435ff335316e00b60d0a7d9c
mai033 <145622463+mai033@users.noreply.github.com>
2023-11-04 11:06:29 -0700

    petController - petModel (#1)

--- server/models/petModel.js
+++ server/models/petModel.js
@@ -1,3 +1,24 @@
 const mongoose = require('mongoose');
 
+const MONGO_URI =
+  'mongodb+srv://meowmatch:letmein123@cluster0.2ullxmv.mongodb.net/?retryWrites=true&w=majority';
+
+mongoose
+  .connect(MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log('Connect to Mongo DB.'))
+  .catch((err) => console.log(err));
+
 const Schema = mongoose.Schema;
+
+const petSchema = new Schema({
+  name: String,
+  age: Number,
+  description: String,
+});
+
+const Pet = mongoose.model('Pet', petSchema);
+
+module.exports = { Pet };
