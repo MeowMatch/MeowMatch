f2abd6936203a5d7b32ed36be97942d6501a8391
mai033 <145622463+mai033@users.noreply.github.com>
2023-11-04 10:52:04 -0700

    petController - petModel

--- server/controllers/petController.js
+++ server/controllers/petController.js
@@ -1,7 +1,24 @@
+const models = require('../models/petModel');
+
 const petController = {};
 
+petController.getPets = async (req, res) => {
+  try {
+    const pets = await models.Pet.find();
+    res.status(200).json(pets);
+  } catch (error) {
+    res.status(404).send('Error fetching pets');
+  }
+};
 
-
-
+petController.addPet = async (req, res) => {
+  const { name, age, description } = req.body;
+  try {
+    const newPet = await models.Pet.create({name, age,description});
+    res.status(201).json(newPet);
+  } catch (error) {
+    res.status(500).send('Error adding pet');
+  }
+};
 
 module.exports = petController;
